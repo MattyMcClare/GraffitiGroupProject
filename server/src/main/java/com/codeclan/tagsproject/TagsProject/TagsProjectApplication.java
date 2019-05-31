f1b7b129788adb325b16b02435d7420f9b1a5d3d
Jon Zarecki <jonzarecki@Jons-MacBook-Air.local>
2019-05-31 09:48:06 +0100

    Spring framework initialised

---
+++ server/src/main/java/com/codeclan/tagsproject/TagsProject/TagsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.tagsproject.TagsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagsProjectApplication.class, args);
+	}
+
+}
