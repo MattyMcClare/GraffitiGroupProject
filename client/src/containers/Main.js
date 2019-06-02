1c2bcdf742f73461772df5e22cf187b55d1f15df
Rob-Marshall87 <marshallrobbie@hotmail.com>
2019-06-02 10:59:16 +0100

    router setup complete

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
-import AllArtView from '../components/AllArtView';
+import React, { Component } from "react";
+import AllArtView from "../components/AllArtView";
+import ErrorPage from "../components/ErrorPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class Main extends Component {
-    render() {
-        return (
-            <AllArtView />
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={AllArtView} />
+          <Route component={ErrorPage} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Main;
