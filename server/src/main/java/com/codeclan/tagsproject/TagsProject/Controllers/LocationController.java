911a4c89a62fb80fcf83d91c089ec325d2824ff8
Jon Zarecki <jonzarecki@Jons-MacBook-Air.local>
2019-06-03 14:57:59 +0100

    calculation written

--- server/src/main/java/com/codeclan/tagsproject/TagsProject/Controllers/LocationController.java
+++ server/src/main/java/com/codeclan/tagsproject/TagsProject/Controllers/LocationController.java
@@ -1,9 +1,15 @@
 package com.codeclan.tagsproject.TagsProject.Controllers;
 
 import com.codeclan.tagsproject.TagsProject.Repositories.LocationRepositories.LocationRepository;
+import com.codeclan.tagsproject.TagsProject.models.Art;
+import com.codeclan.tagsproject.TagsProject.models.Location;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/locations")
@@ -12,6 +18,16 @@ public class LocationController {
     @Autowired
     LocationRepository locationRepository;
 
+    @GetMapping(value = "/long={long}/lat={lat}/dist={dist}")
+    public List<Art> getAllArtWithinDistance (@PathVariable double latitude, double longitude, int distance) {
+        return locationRepository.getAllArtWithinDistance(latitude, longitude, distance);
+    } catch {
+
+    }
 
 
+    @GetMapping
+    public List<Location> getAllLocations() {
+        return locationRepository.findAll();
+    }
 }
