443ebe27087d6b7b9d91462d67b94bdfc327c38c
ericxcross <eric.cross@sympatico.ca>
2019-06-03 10:00:26 +0100

    added advanced options

--- client/src/components/filters/AdvancedFilter.js
+++ client/src/components/filters/AdvancedFilter.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import ToggleDisplay from 'react-toggle-display';
 
-const AdvancedFilter = () => {
-  return (
-    <p>See More Search Options</p>
-  )
+class AdvancedFilter extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+  }
+
+  handleClick() {
+    this.setState({
+      show: !this.state.show
+    });
+  }
+
+  render(){
+    return (
+      <div className="advanced-filter">
+        <button onClick={ () => this.handleClick() }>More Search Options</button>
+        <ToggleDisplay show={this.state.show}>
+          This is the advanced search options.
+        </ToggleDisplay>
+      </div>
+    )
+  }
 }
 
 export default AdvancedFilter
