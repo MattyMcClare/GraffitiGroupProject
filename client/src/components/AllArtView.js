ac4e0e0e310ed04b308f992180cc1ba9e72c9932
Mateusz <matstanczak@gmail.com>
2019-06-03 14:43:28 +0100

    allArtView changed

--- client/src/components/AllArtView.js
+++ client/src/components/AllArtView.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ArtGrid from './ArtGrid';
 import Filter from './filters/Filter';
 import Sort from './sorts/Sort';
 
-class AllArtView extends Component {
-    constructor(props) {
-        super(props);
-        this.locationNotFound = this.locationNotFound.bind(this);
-    }
+const AllArtView = (props) => {
 
-    locationNotFound() {
+    const locationNotFound = () => {
 
     }
 
-    render() {
-        return (
-            <div>
-                <Filter displayLocationNotFound={this.locationNotFound} />
-                <Sort />
-                <ArtGrid />
-            </div>
-        )
-    }
+    return (
+        <div>
+            <Filter displayLocationNotFound={locationNotFound()} />
+            <Sort />
+            <ArtGrid allArt={props.allArt} />
+        </div>
+    )
+
 }
 
 export default AllArtView;
